b050bc75dab4e155a44382f9ee93623f62c8900f
hsandstromOM <hosea@obviouslee.com>
2018-01-19 17:45:37 -0500

    changed paths

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
